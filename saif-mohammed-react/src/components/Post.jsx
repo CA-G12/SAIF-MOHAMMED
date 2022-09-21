b89cd7850f310c9fe694c9a8012ee305852d04ab
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-09-21 21:28:21 +0300

    handle loading, errors and delete posts feature related #8

--- saif-mohammed-react/src/components/Post.jsx
+++ saif-mohammed-react/src/components/Post.jsx
@@ -1,21 +1,31 @@
 import React, { Component } from "react";
 
 class Post extends Component {
-// constructor(props){
-//     super(props)
-//     this.state = {
-//         isComments: props.isComments
-//     }
-// }
+  // constructor(props){
+  //     super(props)
+  //     this.state = {
+  //         isComments: props.isComments
+  //     }
+  // }
 
   render() {
     const { title, body, id } = this.props.post;
 
     return (
-      <div className="post" onClick={() => this.props.handleComments(id)}>
-        <img src={`https://source.unsplash.com/random/${200+id}x${200+id}?islamic`} alt="" />
-        <h3 className="title">{title}</h3>
-        <p className="body">{body}</p>
+      <div className="post">
+        <div onClick={() => this.props.handleComments(id)}>
+          <img
+            src={`https://source.unsplash.com/random/${200 + id}x${
+              200 + id
+            }?islamic`}
+            alt=""
+          />
+          <h3 className="title">{title}</h3>
+          <p className="body">{body}</p>
+        </div>
+        <button onClick={() => this.props.handleDeletePost(id)}>
+          delete post
+        </button>
       </div>
     );
   }
